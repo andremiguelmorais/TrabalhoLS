96eacca41d97c88bd21f6fee2f6072c8690e2dba
andremiguelmorais <105396068+andremiguelmorais@users.noreply.github.com>
2025-04-14 16:16:39 +0100

    Add files via upload

---
+++ Trabalho_Pratico_LS/src/App.js
@@ -0,0 +1,15 @@
+import "./assets/styles/App.css";
+
+function App() {
+  return (
+    <div id="container">
+      <h2>F4 em Linha</h2>
+      Este é o Componente App
+    </div>
+  );
+}
+
+export default App;
+// Esta linha também poderia ser eliminada
+// e adefinição da funsão ser substituida
+// export default function App() {
