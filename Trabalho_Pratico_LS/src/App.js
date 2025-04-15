ccb6bad7baf0459c329aaaac6924ab1f46508bc1
andremiguelmorais <andreribeiromorais20@gmail.com>
2025-04-15 17:31:33 +0100

    app modificada

--- Trabalho_Pratico_LS/src/App.js
+++ Trabalho_Pratico_LS/src/App.js
@@ -1,12 +1,17 @@
 import "./assets/styles/App.css";
+import ControlPanel from "./components/control-panel/control-panel.component";
 
 function App() {
   return (
-    <div id="container">
-      <h2>F4 em Linha</h2>
-      Este é o Componente App
+    <div>
+      <div id="container">
+        <h2>4 em Linhas</h2>
+        Este é o Componente App
+      </div>
+      <ControlPanel />
     </div>
   );
+  
 }
 
 export default App;
